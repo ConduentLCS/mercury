0f4fadeae9e3ee186ca030d4e0ebfad1ec04f557
Andrew Fake <andrew.fake@clcs.conduent.com>
2017-10-20 15:37:28 -0400

    [MERCURY-35] Move TopicExplorer into Dashboard

--- src/components/TopicExplorer.vue
+++ src/components/TopicExplorer.vue
@@ -1,36 +1,43 @@
 <template lang="pug">
-  transition(name="slide")
-    #topic-explorer(:class="{maximized: isMaximized}")
-      .toolbar
-        .details
-          | Topic: #[b {{ this.topic.name }}] -
-          | Offset: #[b {{ this.currentOffset }}]
-        .options
-          i.pause.link.tail.icon(
-            v-if="isTailing"
-            @click="toggleTailing"
-            title="Pause Feed"
-          )
-          i.play.link.tail.icon(
-            v-else
-            @click="toggleTailing"
-            title="Resume Feed"
-          )
-          .ui.icon.input
-            input(type="text" placeholder="Filter..." @input="applyFilter" v-model.trim="filterInput")
-            i.filter.icon(v-if="!filterInput.length")
-            i.link.close.icon(v-else @click="clearFilter")
-          .ui.mini.yellow.circular.icon.button(
-            title="Minimize"
-            @click="minimize"
-          )
-            i.minus.icon
-          .ui.mini.green.circular.icon.button(
-            title="Maximize"
-            @click="maximize"
-          )
-            i.plus.icon
-      .console(:class="{inspect: showViewer}")
+  #topic-explorer(:class="{maximized: isMaximized}")
+    .toolbar
+      .details
+        | Topic: #[b {{ this.topicName }}] -
+        | Offset: #[b {{ this.currentOffset }}]
+      .options
+        i.pause.link.tail.icon(
+          v-if="isTailing"
+          @click="toggleTailing"
+          title="Pause Feed"
+        )
+        i.play.link.tail.icon(
+          v-else
+          @click="toggleTailing"
+          title="Resume Feed"
+        )
+        .ui.icon.input
+          input(type="text" placeholder="Filter..." @input="applyFilter" v-model.trim="filterInput")
+          i.filter.icon(v-if="!filterInput.length")
+          i.link.close.icon(v-else @click="clearFilter")
+        .ui.mini.yellow.circular.icon.button(
+          title="Minimize"
+          @click="minimize"
+          :disabled="!isMaximized"
+          :class="{disabled: !isMaximized}"
+        )
+          i.minus.icon
+        .ui.mini.green.circular.icon.button(
+          title="Maximize"
+          @click="maximize"
+          :disabled="isMaximized"
+          :class="{disabled: isMaximized}"
+        )
+          i.plus.icon
+    .console(:class="{inspect: showViewer, hint: !topic}")
+      div(v-if="!topic")
+        i.fa.fa-send
+        | Select a Topic to Stream
+      template(v-else)
         .list
           .message(
             v-for="message in messages"
@@ -126,6 +133,9 @@
       topic() {
         return this.$store.state.topic;
       },
+      topicName() {
+        return this.topic ? this.topic.name : 'N/A';
+      },
       currentOffset() {
         return this.offset ? this.offset.toLocaleString() : 'N/A';
       },
@@ -206,23 +216,34 @@
 </script>
 
 <style lang="scss">
-  $explorerHeight: 24em;
+  $explorerHeight: 24.3em;
   $toolbarHeight: 2.2em;
   $yellowButton: #FCBF40;
   $greenButton: #34C949;
   $consoleColor: #363636;
   #topic-explorer {
-    position: fixed;
     display: flex;
     flex-direction: column;
     height: $explorerHeight;
     width: 100%;
     bottom: 0;
     background: $consoleColor;
-    box-shadow:0 2px 4px 0 rgba(34,36,38,.12), 
+    -webkit-box-shadow: 0 0 0 1px #d4d4d5, 
+      0 2px 4px 0 rgba(34,36,38,.12),
       0 2px 10px 0 rgba(34,36,38,.15);
-    transition: all 0.5s ease;
-    &.maximized { height: 100% }
+    box-shadow: 0 0 0 1px #d4d4d5,
+      0 2px 4px 0 rgba(34,36,38,.12),
+      0 2px 10px 0 rgba(34,36,38,.15);
+    border-radius: 4px;
+    margin: 3rem 0 0;
+    &.maximized {
+      position: fixed;
+      left: 0;
+      transition: all 0.5s ease;
+      margin: 0;
+      height: 100vh;
+      border-radius: 4px 4px 0 0;
+    }
     .toolbar {
       background: #D5D5D5;
       border: 1px solid #808080;
@@ -232,6 +253,7 @@
       align-items: center;
       justify-content: space-between;
       padding: 0 0.5em;
+      border-radius: 4px;
       .icon.input input {
         padding: 0.3em 1em;
       }
@@ -263,6 +285,19 @@
         &>.viewer { flex-basis: 25% }
         &>.list { flex-basis: 75% }
       }
+      &.hint {
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        height: 8em;
+        color: #FFF;
+        font-size: 2em;
+        i { margin-right: 0.5em }
+        @media(max-width: 425px) {
+          text-align: center;
+          line-height: 2em;
+        }
+      }
       .list {
         overflow-y: auto;
         overflow-x: hidden;
@@ -281,6 +316,7 @@
         flex-direction: column;
         background: #2B2B2B;
         transition: flex-basis 0.3s ease;
+        position: relative;
         .content { 
           flex: 1 0;
           padding: 1.2em 1em 0;
@@ -301,10 +337,11 @@
           padding: 0.1em 0.5em;
         }
         .window.close {
+          color: #FFF;
           display: none;
           position: absolute;
           right: 0.4em;
-          top: 2em;
+          top: 0.3em;
         }
         .json-markup {
           line-height: 1.25em;
@@ -342,15 +379,5 @@
         }
       }
     }
-  }
-  .slide-leave-active,
-  .slide-enter-active {
-    transition: 0.3s;
-  }
-  .slide-enter {
-    transform: translateY(100%);
-  }
-  .slide-leave-to {
-    transform: translateY(100%);
   }
 </style>
